5ee5ead823be79bac4d6373118a14f9a9f144b9c
felipe.guizzo <felipe.guizzo@filadelfiatecnologia.com.br>
2020-03-04 10:26:20 +0000

    starting project

---
+++ MinhaAppAngular/src/app/funcionalidade/funcionalidade.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [
+    // Nossos componentes
+  ],
+
+  imports: [
+    CommonModule
+    // Nossos modulos
+  ],
+  providers: [
+    // Nossos Servicos
+  ]
+
+})
+export class FuncionalidadeModule { }
